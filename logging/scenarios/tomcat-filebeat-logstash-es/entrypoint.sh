1554c92b7941be015b121de0b4e201124c6b2aff
pro-ghanem <pro.ahmedghanem@gmail.com>
2021-04-20 20:58:45 +0200

    Update entrypoint.sh

--- logging/scenarios/tomcat-filebeat-logstash-es/entrypoint.sh
+++ logging/scenarios/tomcat-filebeat-logstash-es/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-#service filebeat start 
-#filebeat -c /etc/filebeat/filebeat.yml &
-
-catalina.sh start
+#!/bin/bash
+catalina.sh start   # must run first otherwise the ci=ontainer will crash
 filebeat -e -c /etc/filebeat/filebeat.yml
-#service filebeat start 
-
-
+#service filebeat start   # didn't work but works inside the container 
+#/etc/init.d/filebeat start    # didn't work but works inside the container 
+#filebeat -e -c /etc/filebeat/filebeat.yml &    # didn't work but works inside the container 
 
