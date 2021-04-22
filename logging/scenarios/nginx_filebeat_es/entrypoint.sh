6c6ae5b984c13e96d44b8f28bcf43695ae70ff15
pro-ghanem <pro.ahmedghanem@gmail.com>
2021-04-22 14:22:50 +0200

    Update entrypoint.sh

--- logging/scenarios/nginx_filebeat_es/entrypoint.sh
+++ logging/scenarios/nginx_filebeat_es/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 catalina.sh start   # must run first otherwise the ci=ontainer will crash
+service nginx start
 filebeat -e -c /etc/filebeat/filebeat.yml
 #service filebeat start   # didn't work
 #/etc/init.d/filebeat start    # didn't work 
